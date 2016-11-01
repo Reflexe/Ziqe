23e848eb1b30634ebcb9d15c57603bffa8ea446d
shrek0 <shmuelhazan0@gmail.com>
2016-11-01 09:04:40 +0200

    Add GlobalMessageStreamFactory

---
+++ Core/Protocol/Object.cpp
@@ -0,0 +1,12 @@
+#include "Object.hpp"
+
+namespace Ziqe {
+namespace Protocol {
+
+Object::Object()
+{
+
+}
+
+} // namespace Protocol
+} // namespace Ziqe
---
+++ Core/Protocol/Object.hpp
@@ -0,0 +1,17 @@
+#ifndef ZIQE_PROTOCOL_OBJECT_HPP
+#define ZIQE_PROTOCOL_OBJECT_HPP
+
+
+namespace Ziqe {
+namespace Protocol {
+
+class Object
+{
+public:
+    Object();
+};
+
+} // namespace Protocol
+} // namespace Ziqe
+
+#endif // ZIQE_PROTOCOL_OBJECT_HPP
