19dc8daaaa53d8737b3a42275838bf92aa4694d5
Shmuel H <shmuelhazan0@gmail.com>
2019-05-04 22:27:25 +0300

    remove dxd

--- Base/Tests/ZqObjectTest.cpp
+++ Base/Tests/ZqObjectTest.cpp
@@ -1,8 +1,8 @@
 /**
- * @file Variant.cpp
+ * @file ZqObjectTest.cpp
  * @author Shmuel Hazan (shmuelhazan0@gmail.com)
  *
- * Ziqe: copyright (C) 2016 Shmuel Hazan
+ * Ziqe: copyright (C) 2017 Shmuel Hazan
  *
  * Ziqe is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,10 +17,3 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "Variant.hpp"
-
-ZQ_BEGIN_NAMESPACE
-namespace Base {
-
-} // namespace Base
-ZQ_END_NAMESPACE
